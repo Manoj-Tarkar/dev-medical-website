2a24a46ce91b52756d80a23b17e6989463b0a70c
Manoj Tarkar <tarkarmanoj2006@gmail.com>
2026-07-13 22:28:28 +0530

    Remove EmailJS debug logs

--- src/services/emailjs.js
+++ src/services/emailjs.js
@@ -3,10 +3,6 @@ import emailjs from '@emailjs/browser'
 const SERVICE_ID = import.meta.env.VITE_EMAILJS_SERVICE_ID
 const TEMPLATE_ID = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
 const PUBLIC_KEY = import.meta.env.VITE_EMAILJS_PUBLIC_KEY
-console.log("SERVICE_ID:", SERVICE_ID)
-console.log("TEMPLATE_ID:", TEMPLATE_ID)
-console.log("PUBLIC_KEY:", PUBLIC_KEY)
-console.log(import.meta.env)
 export async function sendEmail(formData) {
   return emailjs.send(
     SERVICE_ID,
